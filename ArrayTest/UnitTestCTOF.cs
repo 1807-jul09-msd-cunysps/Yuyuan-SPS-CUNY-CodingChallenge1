082d3fc45ab04b03b87ca76ffbf438f1063adf35
Yuyuan Chan <yuyuan17@live.com.my>
2018-07-16 12:04:57 -0400

    Add project files.

---
+++ ArrayTest/UnitTestCTOF.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EmployeeLibrary;
+
+namespace UnitTestEmployeeApp
+{
+    [TestClass]
+    public class UnitTestCTOF
+    {
+        [TestMethod]
+        public void TestCTOF()
+        {
+            // Arrange
+            string celcius = "100";
+            double expectedValue = 212;
+            double actualValue;
+            // Act
+            actualValue = TemperatureConverter.CelsiusToFahrenheit(celcius);
+            // Assert
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+    }
+}
---
+++ ArrayTest/UnitTestFTOC.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EmployeeLibrary;
+
+namespace UnitTestEmployeeApp
+{
+    [TestClass]
+    public class UnitTestFTOC
+    {
+        [TestMethod]
+        public void TestFTOC()
+        {
+            // Arrange
+            string fahrenheit = "212";
+            double expectedValue = 100;
+            double actualValue;
+            // Act
+            actualValue = TemperatureConverter.FahrenheitToCelsius(fahrenheit);
+            // Assert
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+    }
+}
